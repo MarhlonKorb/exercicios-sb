cada0b3538bc164711abb8ad3e808718a56169d1
Marhlon Korb <marhlonkorb@hotmail.com>
2022-06-17 23:26:58 -0300

    Criação de outros metodos http para entender como funciona a injeção de dependências e de como realizar operações envolvendo banco de dados através de solicitações http utilizando conceitos de API RESTFul com Spring Boot.

--- src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
+++ src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,8 +1,24 @@
 package br.com.cod3r.exerciciossb.model.repositories;
 
 import br.com.cod3r.exerciciossb.model.entities.Produto;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
 
+    public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
+
+    //findByNomeContaining
+    //findByNomeISContaining
+    //findByNomeContains
+
+
+    //findByNomeStartsWith
+    //findByNomeEndsWith
+
+    //findByNomeNotContaining
+
+    @Query("Select p FROM p WHERE p.nome LIKE %:nome%")
+    public Iterable<Produto> searchByNameLike(@Param("nome") String nome);
 }
